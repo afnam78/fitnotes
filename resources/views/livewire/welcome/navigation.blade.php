6c992ef468761b31c1be5b5d92e12bf7c0b97a70
Afnan <afnam78@gmail.com>
2025-09-11 11:49:13 +0200

    styles

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="-mx-3 flex flex-1 justify-end">
+<nav>
     @auth
         <a
             href="{{ url('/dashboard') }}"
@@ -7,17 +7,12 @@
             Dashboard
         </a>
     @else
-        <a
-            href="{{ route('login') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-        >
+        <x-secondary-anchor href="{{ route('login') }}">
             Iniciar sesión
-        </a>
+        </x-secondary-anchor>
 
         @if (Route::has('register'))
-            <x-anchor
-                href="{{ route('register') }}"
-            >
+            <x-anchor href="{{ route('register') }}">
                 Crear cuenta
             </x-anchor>
         @endif
