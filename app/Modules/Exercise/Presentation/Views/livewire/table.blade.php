319c8fa03150d24527ca7ac980e2b189b7e772c1
Afnan <afnam78@gmail.com>
2025-09-12 14:39:51 +0200

    Fix filter

--- app/Modules/Exercise/Presentation/Views/livewire/table.blade.php
+++ app/Modules/Exercise/Presentation/Views/livewire/table.blade.php
@@ -19,7 +19,7 @@
            <div class="w-44">
                <x-input-label for="selected_workout_id" :value="__('Entrenamiento')" />
                <select id="selected_workout_id" wire:model.live="selectedWorkout" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                   <option selected>Seleccionar</option>
+                   <option value="">Seleccionar</option>
 
                    @foreach($workouts as $item)
                        <option value="{{$item['id']}}">{{$item['name']}}</option>
