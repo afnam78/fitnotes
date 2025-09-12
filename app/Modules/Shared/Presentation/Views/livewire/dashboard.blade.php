a15a147de4f86ed22d1af784586147bb85594e90
Afnan <afnam78@gmail.com>
2025-09-12 21:32:32 +0200

    Responsive styles

--- app/Modules/Shared/Presentation/Views/livewire/dashboard.blade.php
+++ app/Modules/Shared/Presentation/Views/livewire/dashboard.blade.php
@@ -29,26 +29,41 @@
                 </a>
             @endif
 
-            @if(!empty($getWeeklyRecordsExercises))
-                <div class="text-card-foreground flex flex-col gap-4 p-4 rounded-xl border shadow-sm bg-gradient-to-br from-primary/10 to-accent/10">
-                    <div class="flex items-center justify-between pb-2 border-white border-b-2">
-                        <h3 class="font-semibold text-left">
-                            ¡Récords de la semana!
-                        </h3>
-                        <div class="text-primary">
-                            <x-trophy></x-trophy>
-                        </div>
-                    </div>
-                    @foreach($getWeeklyRecordsExercises as $name)
-                        <div class="flex items-center gap-1 text-sm">
+            <div @class([
+    'grid gap-4',
+    'grid-cols-1 md:grid-cols-2' => !empty($getWeeklyRecordsExercises),
+    'grid-cols-1' => empty($getWeeklyRecordsExercises),
+])>
+                @if(!empty($getWeeklyRecordsExercises))
+                    <div
+                        class="text-card-foreground flex flex-col gap-4 p-4 rounded-xl border shadow-sm bg-gradient-to-br from-primary/10 to-accent/10">
+                        <div class="flex items-center justify-between pb-2 border-white border-b-2">
+                            <h3 class="font-semibold text-left">
+                                ¡Récords de la semana!
+                            </h3>
                             <div class="text-primary">
-                                <x-check-circle></x-check-circle>
+                                <x-trophy></x-trophy>
                             </div>
-                            <p>{{$name}}</p>
                         </div>
-                    @endforeach
+                        @foreach($getWeeklyRecordsExercises as $name)
+                            <div class="flex items-center gap-1 text-sm">
+                                <div class="text-primary">
+                                    <x-check-circle></x-check-circle>
+                                </div>
+                                <p>{{$name}}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+                <div
+                    class="text-card-foreground hidden sm:flex justify-center items-center gap-6 rounded-xl border py-6 shadow-sm bg-gradient-to-br from-primary/10 to-accent/10">
+                    <div data-slot="card-content" class="p-6">
+                        <div class="text-center space-y-2"><p
+                                class="text-sm font-medium italic">{{$motivationalMessage['sentence']}}</p>
+                            <p class="text-xs">- {{$motivationalMessage['author']}}</p></div>
+                    </div>
                 </div>
-            @endif
+            </div>
 
             <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <x-card title="Entrenamientos Hoy" value="{{$workoutsToday}}" description="¡Sigue así!">
@@ -70,7 +85,7 @@
                 </x-card>
             </div>
             <div
-                class="text-card-foreground flex justify-center items-center gap-6 rounded-xl border py-6 shadow-sm bg-gradient-to-br from-primary/10 to-accent/10">
+                class="text-card-foreground sm:hidden flex justify-center items-center gap-6 rounded-xl border py-6 shadow-sm bg-gradient-to-br from-primary/10 to-accent/10">
                 <div data-slot="card-content" class="p-6">
                     <div class="text-center space-y-2"><p
                             class="text-sm font-medium italic">{{$motivationalMessage['sentence']}}</p>
